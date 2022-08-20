f250b4b2e6fd1807cd9260ce4ec93ac5de342e22
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-21 02:59:29 +0500

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,11 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
-  return <main>Dashboard</main>;
+  return (
+    <main>
+      <h2>Dashboard</h2>
+    </main>
+  );
 };
 
 export default Dashboard;
