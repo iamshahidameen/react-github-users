c1f385e32b1cae959332d40c54405796e0399ed0
Shahid Ameen <iamshahidameen@gmail.com>
2022-09-01 20:31:51 +0500

    Setup private route functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
-  console.log(useAuth0());
   const isUser = isAuthenticated && user;
   return (
     <Wrapper>
