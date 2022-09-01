1f85f495b81620847db7c929055b8faeaf65f9cf
Shahid Ameen <iamshahidameen@gmail.com>
2022-09-01 19:54:56 +0500

    Shown login and logout buttons conditionally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,26 @@ const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
   console.log(useAuth0());
+  const isUser = isAuthenticated && user;
   return (
     <Wrapper>
-      <button onClick={loginWithRedirect}>Login</button>
-      <button
-        onClick={() => {
-          return logout({ returnTo: window.location.origin });
-        }}
-      >
-        Logout
-      </button>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            return logout({ returnTo: window.location.origin });
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>Login</button>
+      )}
     </Wrapper>
   );
 };
